72831e9c4f0f685f8fdd34414fd5ebaa888935d6
Julian Meyer <julianmeyer2000@gmail.com>
2018-11-28 13:56:55 -0800

    Add signature scheme and basic tests

--- bls.go
+++ bls.go
@@ -1,18 +1,21 @@
 package bls
 
 import (
+	"bytes"
 	"io"
+	"log"
 	"math/big"
+	"sort"
 )
 
 // Signature is a message signature.
 type Signature struct {
-	s *G1Affine
+	s *G1Projective
 }
 
 // PublicKey is a public key.
 type PublicKey struct {
-	p *G2Affine
+	p *G2Projective
 }
 
 func (g PublicKey) String() string {
@@ -31,12 +34,12 @@ func (g SecretKey) String() string {
 // Sign signs a message with a secret key.
 func Sign(message []byte, key *SecretKey) *Signature {
 	h := HashG1(message).Mul(key.f.n)
-	return &Signature{s: h.ToAffine()}
+	return &Signature{s: h}
 }
 
 // PrivToPub converts the private key into a public key.
 func PrivToPub(k *SecretKey) *PublicKey {
-	return &PublicKey{p: G2AffineOne.Copy().Mul(k.f.n).ToAffine()}
+	return &PublicKey{p: G2AffineOne.Mul(k.f.n)}
 }
 
 // RandKey generates a random secret key.
@@ -58,7 +61,97 @@ func KeyFromBig(i *big.Int) *SecretKey {
 // Verify verifies a signature against a message and a public key.
 func Verify(m []byte, pub *PublicKey, sig *Signature) bool {
 	h := HashG1(m)
-	lhs := Pairing(sig.s.ToProjective(), G2ProjectiveOne.Copy())
-	rhs := Pairing(h, pub.p.ToProjective())
+	lhs := Pairing(sig.s, G2ProjectiveOne)
+	rhs := Pairing(h, pub.p)
+	return lhs.Equals(rhs)
+}
+
+// AggregateSignatures adds up all of the signatures.
+func AggregateSignatures(s []*Signature) *Signature {
+	newSig := &Signature{s: G1ProjectiveZero}
+	for _, sig := range s {
+		newSig.Aggregate(sig)
+	}
+	return newSig
+}
+
+// Aggregate adds one signature to another
+func (s *Signature) Aggregate(other *Signature) {
+	newS := s.s.Add(other.s)
+	s.s = newS
+}
+
+// NewAggregateSignature creates a blank aggregate signature.
+func NewAggregateSignature() *Signature {
+	return &Signature{s: G1ProjectiveZero.Copy()}
+}
+
+// implement `Interface` in sort package.
+type sortableByteArray [][]byte
+
+func (b sortableByteArray) Len() int {
+	return len(b)
+}
+
+func (b sortableByteArray) Less(i, j int) bool {
+	// bytes package already implements Comparable for []byte.
+	switch bytes.Compare(b[i], b[j]) {
+	case -1:
+		return true
+	case 0, 1:
+		return false
+	default:
+		log.Panic("not fail-able with `bytes.Comparable` bounded [-1, 1].")
+		return false
+	}
+}
+
+func (b sortableByteArray) Swap(i, j int) {
+	b[j], b[i] = b[i], b[j]
+}
+
+func sortByteArrays(src [][]byte) [][]byte {
+	sorted := sortableByteArray(src)
+	sort.Sort(sorted)
+	return sorted
+}
+
+// VerifyAggregate verifies each public key against each message.
+func (s *Signature) VerifyAggregate(pubKeys []*PublicKey, msgs [][]byte) bool {
+	if len(pubKeys) != len(msgs) {
+		return false
+	}
+
+	// messages must be distinct
+	msgsSorted := sortByteArrays(msgs)
+	lastMsg := []byte(nil)
+
+	// check for duplicates
+	for _, m := range msgsSorted {
+		if bytes.Equal(m, lastMsg) {
+			return false
+		}
+		lastMsg = m
+	}
+
+	lhs := Pairing(s.s, G2ProjectiveOne)
+	rhs := FQ12One.Copy()
+	for i := range pubKeys {
+		h := HashG1(msgs[i])
+		rhs.MulAssign(Pairing(h, pubKeys[i].p))
+	}
+	return lhs.Equals(rhs)
+}
+
+// VerifyAggregateCommon verifies each public key against a message.
+// This is vulnerable to rogue public-key attack. Each user must
+// provide a proof-of-knowledge of the public key.
+func (s *Signature) VerifyAggregateCommon(pubKeys []*PublicKey, msg []byte) bool {
+	h := HashG1(msg)
+	lhs := Pairing(s.s, G2ProjectiveOne)
+	rhs := FQ12One.Copy()
+	for _, p := range pubKeys {
+		rhs.MulAssign(Pairing(h, p.p))
+	}
 	return lhs.Equals(rhs)
 }
--- bls_test.go
+++ bls_test.go
@@ -1,18 +1,163 @@
 package bls_test
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/phoreproject/bls"
 )
 
-func TestBasicAcceptance(t *testing.T) {
+func SignVerify(loopCount int) error {
 	r := NewXORShift(1)
-	priv, _ := bls.RandKey(r)
-	pub := bls.PrivToPub(priv)
-	msg := []byte("Hello world!")
-	sig := bls.Sign(msg, priv)
-	if !bls.Verify(msg, pub, sig) {
-		t.Fatal("sig did not verify")
+	for i := 0; i < loopCount; i++ {
+		priv, _ := bls.RandKey(r)
+		pub := bls.PrivToPub(priv)
+		msg := []byte(fmt.Sprintf("Hello world! 16 characters %d", i))
+		sig := bls.Sign(msg, priv)
+		if !bls.Verify(msg, pub, sig) {
+			return errors.New("sig did not verify")
+		}
+	}
+	return nil
+}
+
+func SignVerifyAggregateCommonMessage(loopCount int) error {
+	r := NewXORShift(2)
+	pubkeys := make([]*bls.PublicKey, 0, 1000)
+	sigs := make([]*bls.Signature, 0, 1000)
+	msg := []byte(">16 character identical message")
+	for i := 0; i < loopCount; i++ {
+		priv, _ := bls.RandKey(r)
+		pub := bls.PrivToPub(priv)
+		sig := bls.Sign(msg, priv)
+		pubkeys = append(pubkeys, pub)
+		sigs = append(sigs, sig)
+		if i < 10 || i > (loopCount-5) {
+			newSig := bls.AggregateSignatures(sigs)
+			if !newSig.VerifyAggregateCommon(pubkeys, msg) {
+				return errors.New("sig did not verify")
+			}
+		}
+	}
+	return nil
+}
+
+func SignVerifyAggregateCommonMessageMissingSig(loopCount int) error {
+	r := NewXORShift(3)
+	skippedSig := loopCount / 2
+	pubkeys := make([]*bls.PublicKey, 0, 1000)
+	sigs := make([]*bls.Signature, 0, 1000)
+	msg := []byte(">16 character identical message")
+	for i := 0; i < loopCount; i++ {
+		priv, _ := bls.RandKey(r)
+		pub := bls.PrivToPub(priv)
+		sig := bls.Sign(msg, priv)
+		pubkeys = append(pubkeys, pub)
+		if i != skippedSig {
+			sigs = append(sigs, sig)
+		}
+		if i < 10 || i > (loopCount-5) {
+			newSig := bls.AggregateSignatures(sigs)
+			if newSig.VerifyAggregateCommon(pubkeys, msg) != (i < skippedSig) {
+				return errors.New("sig did not verify")
+			}
+		}
+	}
+	return nil
+}
+
+func AggregateSignatures(loopCount int) error {
+	r := NewXORShift(4)
+	pubkeys := make([]*bls.PublicKey, 0, 1000)
+	msgs := make([][]byte, 0, 1000)
+	sigs := make([]*bls.Signature, 0, 1000)
+	for i := 0; i < loopCount; i++ {
+		priv, _ := bls.RandKey(r)
+		pub := bls.PrivToPub(priv)
+		msg := []byte(fmt.Sprintf(">16 character identical message %d", i))
+		sig := bls.Sign(msg, priv)
+		pubkeys = append(pubkeys, pub)
+		msgs = append(msgs, msg)
+		sigs = append(sigs, sig)
+
+		if i < 10 || i > (loopCount-5) {
+			newSig := bls.AggregateSignatures(sigs)
+			if !newSig.VerifyAggregate(pubkeys, msgs) {
+				return errors.New("sig did not verify")
+			}
+		}
+	}
+	return nil
+}
+
+func TestSignVerify(t *testing.T) {
+	err := SignVerify(10)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSignVerifyAggregateCommon(t *testing.T) {
+	err := SignVerifyAggregateCommonMessage(10)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSignVerifyAggregateCommonMissingSig(t *testing.T) {
+	err := SignVerifyAggregateCommonMessageMissingSig(10)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSignAggregateSigs(t *testing.T) {
+	err := AggregateSignatures(10)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestAggregateSignaturesDuplicatedMessages(t *testing.T) {
+	r := NewXORShift(5)
+
+	pubkeys := make([]*bls.PublicKey, 0, 1000)
+	msgs := make([][]byte, 0, 1000)
+	sigs := bls.NewAggregateSignature()
+
+	key, _ := bls.RandKey(r)
+	pub := bls.PrivToPub(key)
+	message := []byte(">16 char first message")
+	sig := bls.Sign(message, key)
+	pubkeys = append(pubkeys, pub)
+	msgs = append(msgs, message)
+	sigs.Aggregate(sig)
+
+	if !sigs.VerifyAggregate(pubkeys, msgs) {
+		t.Fatal("signature does not verify")
+	}
+
+	key2, _ := bls.RandKey(r)
+	pub2 := bls.PrivToPub(key2)
+	message2 := []byte(">16 char second message")
+	sig2 := bls.Sign(message2, key2)
+	pubkeys = append(pubkeys, pub2)
+	msgs = append(msgs, message2)
+	sigs.Aggregate(sig2)
+
+	if !sigs.VerifyAggregate(pubkeys, msgs) {
+		t.Fatal("signature does not verify")
+	}
+
+	key3, _ := bls.RandKey(r)
+	pub3 := bls.PrivToPub(key3)
+	sig3 := bls.Sign(message2, key3)
+	pubkeys = append(pubkeys, pub3)
+	msgs = append(msgs, message2)
+	sigs.Aggregate(sig3)
+
+	if sigs.VerifyAggregate(pubkeys, msgs) {
+		t.Fatal("signature verifies with duplicate message")
 	}
 }
