da95d4798b09e9f45a29dc53124b2a0b4c1dfc13
nisdas <nishdas93@gmail.com>
2019-08-21 11:54:52 +0530

    add benchmark

--- g1pubs/verify_benchmark_test.go
+++ g1pubs/verify_benchmark_test.go
@@ -2,6 +2,7 @@ package g1pubs_test
 
 import (
 	"crypto/rand"
+	"fmt"
 	"github.com/phoreproject/bls/g1pubs"
 	"testing"
 )
@@ -46,10 +47,38 @@ func BenchmarkVerifyAggregateCommonWithDomain(b *testing.B) {
 	}
 
 	aggSig := g1pubs.AggregateSignatures(sigs)
-
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		if !aggSig.VerifyAggregateCommonWithDomain(pks, msg, domain) {
+			b.Fatal("could not verify aggregate sig")
+		}
+	}
+}
+
+func BenchmarkVerifyAggregateMultipleWithDomain(b *testing.B) {
+	sigN := 128
+	var msg [32]byte
+	var msgs [][32]byte
+	domain := [8]byte{42}
+
+	var sigs []*g1pubs.Signature
+	var pks []*g1pubs.PublicKey
+	for i := 0; i < sigN; i++ {
+		sk, err := g1pubs.RandKey(rand.Reader)
+		if err != nil {
+			b.Fatal(err)
+		}
+		copy(msg[:], fmt.Sprintf("Some message %d", i))
+		sigs = append(sigs, g1pubs.SignWithDomain(msg, sk, domain))
+		pks = append(pks, g1pubs.PrivToPub(sk))
+		msgs = append(msgs, msg)
+	}
+
+	aggSig := g1pubs.AggregateSignatures(sigs)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if !aggSig.VerifyAggregateWithDomain(pks, msgs, domain) {
 			b.Fatal("could not verify aggregate sig")
 		}
 	}
